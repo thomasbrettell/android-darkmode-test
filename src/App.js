5241cc986ee65c8b83fa8c8d33d7c69687438d0a
thomasbrettell <70102377+thomasbrettell@users.noreply.github.com>
2021-08-30 11:31:08 +1000

    Add google example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Checkbox from './components/Checkbox/Checkbox';
 import StyledCheckboxOne from './components/StyledCheckboxOne/StyledCheckboxOne';
-import styles from './App.module.css'
+import styles from './App.module.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       </div>
       <div className={styles.item}>
         <StyledCheckboxOne />
+      </div>
+      <div className={styles.item}>
+        <Button variant="outlined">Default</Button>
       </div>
     </div>
   );
---
+++ src/components/StyledCheckboxTwo/StyledCheckboxTwo.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import styles from './StyledCheckboxOne.module.css'
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const StyledCheckboxTwo = () => {
+  const [checked, setCheck] = useState(false)
+
+  const className = cx({
+    StyledCheckboxTwo: true,
+    checked: checked,
+  });
+
+  const checkHandler = (e) => {
+    setCheck(!checked)
+  }
+
+  return (
+    <label className={className}>
+      <input type="checkbox" onChange={checkHandler} value='Checkbox'></input>
+      <span>Checkbox</span>
+    </label>
+  );
+};
+
+export default StyledCheckboxTwo;
